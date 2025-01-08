a1cb673a6ec050e70563c5f80d82738f04e5262a
Junfa Li <1170640485fg@gmail.com>
2025-01-07 20:55:40 -0500

    The route design has been completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import router from './router/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router} />
 )
