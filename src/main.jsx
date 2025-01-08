8a696ae01d2b0893201217918763746b95091f36
Junfa Li <1170640485fg@gmail.com>
2025-01-08 15:23:41 -0500

    Set up the redux for billlist and updated the json server

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import './index.css'
 import App from './App.jsx'
 import router from './router/index.jsx'
 
+import store from './store/index.jsx'
+import { Provider } from 'react-redux'
+
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
 import { Outlet } from "react-router-dom"
+import { getBillList } from "../../store/modules/billStore"
 
 const Layout = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getBillList())
+    }, [dispatch])
     return (
         <div>
             <Outlet />
